71854b0f91466428dc2dcf1b1a0de28eda1a2434
Carl Joshua Asuliz <asulizc@gmail.com>
2026-03-13 20:02:25 +0800

    Rollback to e43cc3d: Restoring proper API routing

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // Add this line to fix pathing issues on refresh
-  plugins: [
-    react(),
-    tailwindcss(),
+  plugins: [react(),
+  tailwindcss(),
   ],
   server: {
     proxy: {
